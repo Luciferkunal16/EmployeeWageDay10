b05bbd1c6c36ecc830256371ea6d719de5a99ded
Luciferkunal16 <Kunalbatham15@gmail.com>
2021-11-01 18:53:15 +0530

    Added UC10 : Can store Diffrent Object in Array

--- src/Oops_Employee_Wage.java
+++ src/Oops_Employee_Wage.java
@@ -5,25 +5,32 @@ import java.util.Scanner;
 public class Oops_Employee_Wage {
 public static final int IS_PART_TIME=1;
 public static final int IS_FULL_TIME=2;
-public static  int EMP_RATE_PER_HOUR;
-public static  int NUM_OF_WORKING_DAYS;
-public static  int MAX_HRS;
-public   String companyName ;
-public int totalEmpWage;
-
-
- public Oops_Employee_Wage(String company,int empRatePerHour,int numofWorkingDays,int maxHrs) {
-	companyName=company;
-	EMP_RATE_PER_HOUR=empRatePerHour;
-	NUM_OF_WORKING_DAYS=numofWorkingDays;
-	MAX_HRS=maxHrs;
+int numOfCompany=0;
+private EmpWageOfCompany[] companyWageArray;
+public  Oops_Employee_Wage(){
+	companyWageArray=new EmpWageOfCompany[5];
 }
 
- void computeWage() {
+
+void addCompanyEmpWage(String company, int empRatePerHour,int numOfWorkingDays, int maxHRs) {
+	companyWageArray[numOfCompany]=new EmpWageOfCompany(company, empRatePerHour, numOfWorkingDays, maxHRs);
+	numOfCompany++;
+	
+}
+void computeEmpWage() {
+	for (int i=0;i<numOfCompany;i++) {
+		companyWageArray[i].setTotalEmpWage(computeWage(companyWageArray[i]));
+	}
+}
+
+
+
+
+ int computeWage(EmpWageOfCompany companyempwage) {
 	int empHrs=0;
 	int totalEmpHrs=0;
 	int totalWorkingDays=0;
-	while (totalEmpHrs <=MAX_HRS && totalWorkingDays <NUM_OF_WORKING_DAYS) {
+	while (totalEmpHrs <=companyempwage.MAX_HRS&& totalWorkingDays <companyempwage.NUM_OF_WORKING_DAYS) {
 		totalWorkingDays++;
 		int attendence=(int)((Math.random()*10)%3);
 		switch (attendence) {
@@ -43,24 +50,20 @@ public int totalEmpWage;
 	
 	
 		}
-		totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
+		return totalEmpHrs*companyempwage.EMP_RATE_PER_HOUR;
 		
 	
 	
 	
 }
- public String toString() {
-	 return "Total Emp Wage for Company "+companyName+" is "+totalEmpWage;
- }
+
 public static void main(String[] args) {
 	
-	Oops_Employee_Wage reliance=new Oops_Employee_Wage("Reliance", 20, 30, 10);
-	Oops_Employee_Wage Airtel=new Oops_Employee_Wage("Airtel", 12, 2, 8);
-	reliance.computeWage();
-	System.out.println(reliance.toString());
-	Airtel.computeWage();
-	System.out.println(Airtel);
-	
+	Oops_Employee_Wage empWageObj=new Oops_Employee_Wage();
+	empWageObj.addCompanyEmpWage("Reliance", 20, 6, 12);
+	empWageObj.addCompanyEmpWage("Airtel", 10, 2, 2);
+	empWageObj.computeEmpWage();
+
 	
 	
 	
